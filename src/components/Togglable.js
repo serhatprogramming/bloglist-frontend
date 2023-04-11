cb947279efc0e83de6f093f6009122b553c983f6
serhatprogramming <serhatprogramming@gmail.com>
2023-04-11 14:30:12 -0400

    proptypes added

--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -1,4 +1,5 @@
 import { useState, forwardRef, useImperativeHandle } from "react";
+import PropTypes from "prop-types";
 
 const Togglable = forwardRef(({ children, buttonLabel }, ref) => {
   const [visible, setVisible] = useState(false);
@@ -26,5 +27,11 @@ const Togglable = forwardRef(({ children, buttonLabel }, ref) => {
     </div>
   );
 });
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
+
+Togglable.displayName = "Togglable";
 
 export default Togglable;
