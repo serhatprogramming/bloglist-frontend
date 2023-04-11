d9e793d8ba35e276db639b0c5a72a70c1a94514a
serhatprogramming <serhatprogramming@gmail.com>
2023-04-11 19:27:43 -0400

    toggled view unit test

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,22 +1,38 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
-import "@testing-library/jest-dom/extend-expect";
 
 describe("Blog tests", () => {
-  test("renders only title and author not likes nor url", async () => {
+  let container;
+
+  beforeEach(() => {
     const blog = {
       title: "test title",
       author: "test author",
       url: "test.com",
       likes: 0,
+      user: { username: "testuser" },
     };
 
-    const { container } = render(<Blog blog={blog} />);
+    container = render(<Blog blog={blog} username="user" />).container;
+  });
+
+  test("renders only title and author not likes nor url", async () => {
     const div = container.querySelector(".blogContent");
     expect(div).toHaveTextContent("test title");
     expect(div).not.toHaveTextContent("likes");
     expect(div).not.toHaveTextContent("test.com");
+  });
+
+  test("url and likes are visible when view button is clicked", async () => {
+    const user = userEvent.setup();
+    const div = container.querySelector(".blogContent");
+    expect(div).not.toHaveTextContent("test.com");
+    const viewButton = screen.getByText("view");
+    screen.debug(viewButton);
+    await user.click(viewButton);
+    expect(div).toHaveTextContent("test.com");
   });
 });
