e063f6cd7633035dfb96b02ad4f3cd2d8eb66e6b
serhatprogramming <serhatprogramming@gmail.com>
2023-04-11 13:11:46 -0400

    blog card functionality added

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleShow = () => setShow(!show);
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{" "}
+      <button onClick={toggleShow}>{show ? "hide" : "view"}</button>
+      {show && (
+        <>
+          <br />
+          {blog.url}
+          <br />
+          likes {blog.likes} <button>like</button>
+          <br />
+          {blog.user.username}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
