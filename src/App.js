621c36143fa4817766ba508636be70a9ac451c49
serhatprogramming <serhatprogramming@gmail.com>
2023-04-11 13:50:23 -0400

    sort by likes count

--- src/App.js
+++ src/App.js
@@ -71,9 +71,11 @@ const App = () => {
         <button onClick={handleLogout}>logout</button>{" "}
       </p>
       {createNewBlog()}
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} token={token} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} token={token} />
+        ))}
     </div>
   );
   //======================================================//
