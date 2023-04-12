782bbf6c69d7a6ec5e4908881a1e046d168c544d
serhatprogramming <serhatprogramming@gmail.com>
2023-04-12 18:43:25 -0400

    test db login and create a blog

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,23 +1,30 @@
 /* eslint-disable no-undef */
 describe("Blog App", function () {
   beforeEach(function () {
+    cy.request("POST", "http://localhost:3003/api/testing/reset");
+    const user = {
+      name: "Helsinki Finn",
+      username: "helsinki",
+      password: "helsinki",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/", user);
     cy.visit("http://localhost:3000");
   });
 
-  it("front page can be opened", function () {
+  it("login form shown", function () {
     cy.contains("login to application");
   });
 
   it("login credentials", function () {
-    cy.get("#username").type("Helsinki");
+    cy.get("#username").type("helsinki");
     cy.get("#password").type("helsinki");
     cy.get("#login-button").click();
-    cy.contains("Helsinki is logged in");
+    cy.contains("helsinki is logged in");
   });
 
   describe("when logged in", function () {
     beforeEach(function () {
-      cy.get("#username").type("Helsinki");
+      cy.get("#username").type("helsinki");
       cy.get("#password").type("helsinki");
       cy.get("#login-button").click();
     });
@@ -26,7 +33,8 @@ describe("Blog App", function () {
       cy.get("#title").type("cypress title");
       cy.get("#author").type("cypress author");
       cy.get("#url").type("cypress.url.com");
-      // cy.get("#create-button").click();
+      cy.get("#create-button").click();
+      cy.contains("cypress title");
     });
   });
 });
