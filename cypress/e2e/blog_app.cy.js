63477e7be760edf22ebd9bdee1b90481dfbad3d7
serhatprogramming <serhatprogramming@gmail.com>
2023-04-12 18:55:49 -0400

    like button test

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -14,27 +14,45 @@ describe("Blog App", function () {
   it("login form shown", function () {
     cy.contains("login to application");
   });
-
-  it("login credentials", function () {
-    cy.get("#username").type("helsinki");
-    cy.get("#password").type("helsinki");
-    cy.get("#login-button").click();
-    cy.contains("helsinki is logged in");
-  });
-
-  describe("when logged in", function () {
-    beforeEach(function () {
+  describe("login", function () {
+    it("succeeds with correct credentials", function () {
       cy.get("#username").type("helsinki");
       cy.get("#password").type("helsinki");
       cy.get("#login-button").click();
+      cy.contains("helsinki is logged in");
     });
-    it("a new blog can be created", function () {
-      cy.contains("new note").click();
-      cy.get("#title").type("cypress title");
-      cy.get("#author").type("cypress author");
-      cy.get("#url").type("cypress.url.com");
-      cy.get("#create-button").click();
-      cy.contains("cypress title");
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("XXXX");
+      cy.get("#password").type("XXXX");
+      cy.get("#login-button").click();
+      cy.contains("Wrong Credentials");
+    });
+
+    describe("when logged in", function () {
+      beforeEach(function () {
+        cy.get("#username").type("helsinki");
+        cy.get("#password").type("helsinki");
+        cy.get("#login-button").click();
+      });
+      it("A blog can be created", function () {
+        cy.contains("new note").click();
+        cy.get("#title").type("cypress title");
+        cy.get("#author").type("cypress author");
+        cy.get("#url").type("cypress.url.com");
+        cy.get("#create-button").click();
+        cy.contains("cypress title");
+      });
+      it("Users can like a blog", function () {
+        cy.contains("new note").click();
+        cy.get("#title").type("cypress title");
+        cy.get("#author").type("cypress author");
+        cy.get("#url").type("cypress.url.com");
+        cy.get("#create-button").click();
+        cy.contains("cypress title");
+        cy.contains("view").click();
+        cy.contains("like").click();
+        cy.contains("likes 1");
+      });
     });
   });
 });
